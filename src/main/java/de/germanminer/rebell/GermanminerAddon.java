981739c0405f0f81de1f86a42df289b5015023e4
UnbekannterRebel <76190841+UnbekannterRebel@users.noreply.github.com>
2020-12-22 15:36:04 +0100

    Add files via upload

--- src/main/java/de/germanminer/rebell/GermanminerAddon.java
+++ src/main/java/de/germanminer/rebell/GermanminerAddon.java
@@ -1,6 +1,7 @@
 package de.germanminer.rebell;
 
 import net.labymod.api.LabyModAddon;
+import net.labymod.api.events.MessageReceiveEvent;
 import net.labymod.api.events.MessageSendEvent;
 import net.labymod.main.LabyMod;
 import net.labymod.settings.elements.BooleanElement;
@@ -9,14 +10,17 @@ import net.labymod.settings.elements.SettingsElement;
 import net.labymod.utils.Consumer;
 import net.labymod.utils.Material;
 import net.labymod.utils.ServerData;
+import net.minecraft.util.text.TextFormatting;
 
 import java.util.List;
 
 public class GermanminerAddon extends LabyModAddon {
 
-    private boolean enabled = false;
+    private boolean badfrak = false;
+    private boolean failblocker = false;
 
-    final String message4 = ("§c[Rebells Kek Addon]§6" + " Der Fail blocker hat dich gerettet :D");
+    final String prefix = (TextFormatting.DARK_RED + "[Rebells Kek Addon] ");
+    final String failblockermsg = (prefix + TextFormatting.GOLD + "Der Fail blocker hat dich gerettet :D");
 
 
 
@@ -29,7 +33,7 @@ public class GermanminerAddon extends LabyModAddon {
 
             public void accept(final ServerData serverData) {
                 if(LabyMod.getInstance().getPlayerName() != null) {
-                    LabyMod.getInstance().displayMessageInChat("§6Du spielst aktuell auf dem server: " + serverData.getIp());
+                    LabyMod.getInstance().displayMessageInChat(TextFormatting.GOLD + "Du spielst aktuell auf dem server: " + serverData.getIp());
 
                 }
             }
@@ -40,17 +44,17 @@ public class GermanminerAddon extends LabyModAddon {
 
             public boolean onSend(final String message) {
 
-                if(enabled) {
+                if(failblocker) {
                     final String message2;
                     message2 = message;
                     final String message3 = message2.toLowerCase();
 
                     if (message3.startsWith("7rs") || message3.startsWith("7h") || message3.startsWith("7m") || message3.startsWith("7menu")
                             || message3.startsWith("7vital") || message3.startsWith("7fett") || message3.startsWith("7ticket") || message3.startsWith("7sichern")
-                            || message3.startsWith("7sicherung") || message3.startsWith("7baurechte") || message3.startsWith("7quests")
+                            || message3.startsWith("7sicherung") || message3.startsWith("7baurechte") || message3.startsWith("7quests") || message3.startsWith("7ot")
                             || message3.startsWith("7weih")) { //Bei ende vom weihnachts event wieder entfernen!
 
-                        LabyMod.getInstance().displayMessageInChat(message4);
+                        LabyMod.getInstance().displayMessageInChat(failblockermsg);
                         return true;
                     }
                 }
@@ -58,6 +62,51 @@ public class GermanminerAddon extends LabyModAddon {
             }
 
     });
+
+        this.getApi().getEventManager().register(new MessageReceiveEvent()  {
+            @Override
+            public boolean onReceive(String s, String s1) {
+                if (badfrak) {
+                    if (s1.contains("[FUNK]")) {
+                        if (s1.contains("ACHTUNG! Die gegnerische Fraktion versucht derzeit das Gebiet")) {
+                            if (s1.contains("ACHTUNG! Die gegnerische Fraktion versucht derzeit das Gebiet News-Zentrale")) {
+                                LabyMod.getInstance().displayMessageInChat(TextFormatting.DARK_RED + "Taxipunkt: News Zentrale");
+                            } else {
+                                if (s1.contains("ACHTUNG! Die gegnerische Fraktion versucht derzeit das Gebiet Skatepark Harrenheim")) {
+                                    LabyMod.getInstance().displayMessageInChat(TextFormatting.DARK_RED + "Taxipunkt: Skatepark");
+                                } else {
+                                    if (s1.contains("ACHTUNG! Die gegnerische Fraktion versucht derzeit das Gebiet F+G Fabrikhalle")) {
+                                        LabyMod.getInstance().displayMessageInChat(TextFormatting.DARK_RED + "Taxipunkt: Spring Spring Hopp");
+                                    } else {
+                                        if (s1.contains("ACHTUNG! Die gegnerische Fraktion versucht derzeit das Gebiet Alte Zollstelle")) {
+                                            LabyMod.getInstance().displayMessageInChat(TextFormatting.DARK_RED + "Taxipunkt: Alte Zollstelle");
+                                        } else {
+                                            if (s1.contains("ACHTUNG! Die gegnerische Fraktion versucht derzeit das Gebiet Friedhof")) {
+                                                LabyMod.getInstance().displayMessageInChat(TextFormatting.DARK_RED + "Taxipunkt: Friedhof");
+                                            } else {
+                                                if (s1.contains("ACHTUNG! Die gegnerische Fraktion versucht derzeit das Gebiet Tal")) {
+                                                    LabyMod.getInstance().displayMessageInChat(TextFormatting.DARK_RED + "Taxipunkt: Medic Tal <3");
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        } else {
+                            //hier kommen halt andere funk nachrichten hin.
+                        }
+                    } else {
+                        //hier wird alles bearbeitet, was kein funk ist.
+                    }
+            } else {
+                    //wenn mann nicht im bad-frak modus ist
+                }
+                return false;
+            }
+        });
+
+
+
 
     }
 
@@ -68,22 +117,34 @@ public class GermanminerAddon extends LabyModAddon {
 
     @Override
     public void loadConfig() {
-        this.enabled = !this.getConfig().has("enabled") || this.getConfig().get("enabled").getAsBoolean();
+        this.failblocker = !this.getConfig().has("failblocker") || this.getConfig().get("failblocker").getAsBoolean();
+        this.badfrak = !this.getConfig().has("badfrak") || this.getConfig().get("badfrak").getAsBoolean();
     }
 
     @Override
     protected void fillSettings(final List<SettingsElement> list) {
         final BooleanElement booleanElement = new BooleanElement("Fail blocker", new ControlElement.IconData(Material.BARRIER), new Consumer<Boolean>() {
 
-            public void accept(final Boolean enabled) {
-                GermanminerAddon.this.enabled = enabled;
+            public void accept(final Boolean failblocker) {
+                GermanminerAddon.this.failblocker = failblocker;
 
-                GermanminerAddon.this.getConfig().addProperty("enabled", enabled);
+                GermanminerAddon.this.getConfig().addProperty("failblocker", failblocker);
                 GermanminerAddon.this.saveConfig();
             }
-        }, this.enabled);
+        }, this.failblocker);
+
+        final BooleanElement booleanElement1 = new BooleanElement("GW-Taxipunkt", new ControlElement.IconData(Material.DIAMOND_SWORD), new Consumer<Boolean>() {
+
+            public void accept(Boolean badfrak) {
+                GermanminerAddon.this.badfrak = badfrak;
+
+                GermanminerAddon.this.getConfig().addProperty("badfrak", badfrak);
+                GermanminerAddon.this.saveConfig();
+            }
+        }, this.badfrak);
 
         list.add(booleanElement);
+        list.add(booleanElement1);
     }
 
 
